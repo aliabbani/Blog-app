dbbbbadabaa10721978fa5750554ccee8d4c1386
ali abbani <aliox@ahlam.com>
2021-12-01 11:06:53 +0200

    Add 3 generator controller, add 3 show html file inside the view, create the route for the pages, and fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "pages#index"
+
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
