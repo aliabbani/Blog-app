49b75f871667efc8336e28f0f2b9b81def4dc822
ali abbani <aliox@ahlam.com>
2021-12-03 13:25:52 +0200

    Create immigration for posts, comments, and likes

--- db/migrate/20211203102025_create_users.rb
+++ db/migrate/20211203102025_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :photo
       t.string :bio
-      t.integer :PostsCounter
+      t.integer :postsCounter
 
       t.timestamps
     end
