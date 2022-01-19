29ba6689495929e43222155acde35cc4fb206bef
ali abbani <aliox@ahlam.com>
2022-01-19 22:54:30 +0200

    add basic functionalit to the users/id/posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     @posts = Post.all
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 end
