7816e73d54ca5aa5d6e704d3bd70329dcd98ea48
ali abbani <aliox@ahlam.com>
2022-01-20 18:52:08 +0200

    Create a user partial, and edit the views/users/index file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,32 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    # new object from params
+    post = Post.new(params.require(:post).permit(:question, :answer, :distractor_1, :distractor_2))
+    # respond_to block
+    respond_to do |format|
+        format.html do
+            if post.save
+                # success message
+                flash[:success] = "Question saved successfully"
+                # redirect to index
+                redirect_to user_posts_url
+            else
+                # error message
+                flash.now[:error] = "Error: Question could not be saved"
+                # render new
+                render :new, locals: { post: post }
+            end
+        end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 wendy = User.create!(name: 'Wendy', photo: 'image2' , bio: 'lorem40', posts_counter: 0)
 john = User.create!(name: 'John', photo: 'image3', bio: 'lorem40', posts_counter: 0)
+dipesh = User.create!(name: 'Dipesh', photo: 'image3' , bio: 'lorem40', posts_counter: 0)
+abbani = User.create!(name: 'Abbani', photo: 'image4', bio: 'lorem40', posts_counter: 0)
+
 
 post1 = wendy.posts.create!(title: 'Cats and Dogs', text: 'this is first post about cats and dogs!', comments_counter: 0, likes_counter: 0)
 post2 = john.posts.create!(title: 'Never ending fun', text: 'this is second post but I don\'t think I should create another post again', comments_counter: 0, likes_counter: 0)
