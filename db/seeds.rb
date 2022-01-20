ae36352871cfe298c03da55311d121cb37178af1
ali abbani <aliox@ahlam.com>
2022-01-20 11:44:46 +0200

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+wendy = User.create!(name: 'Wendy', photo: image2 , bio: lorem40, posts_counter: 0)
+john = User.create!(name: 'John', photo: image3, bio: lorem40, posts_counter: 0)
+
+post1 = michael.posts.create!(title: 'Cats and Dogs', text: 'this is first post about cats and dogs!', comments_counter: 0, likes_counter: 0)
+post2 = john.posts.create!(title: 'Never ending fun', text: 'this is second post but I don\'t think I should create another post again', comments_counter: 0, likes_counter: 0)
+
+wendy.comments.create!(text: 'really nice post Michael', post: post1)
+john.comments.create!(text: 'Well Done 👏👏👏', post: post1)
+john.comments.create!(text: 'Love 💓 it!!!', post: post1)
