bc854d066cc1698446c932a91513f2414c74861c
ali abbani <aliox@ahlam.com>
2022-01-21 10:39:35 +0200

    Update the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root "users#index"
   # post 'posts', to: 'posts#create' # create
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create]
+  end
+
+  resources :posts do
+    resources :comments, only: [create]
+    resources :likes, only: [:create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
