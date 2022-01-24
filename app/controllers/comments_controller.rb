04088c937e878916b926066bf50cbc57fee8b83b
ali abbani <aliox@ahlam.com>
2022-01-24 12:45:37 +0200

    Changes on the form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,19 +4,19 @@ class CommentsController < ApplicationController
   # end
 
   def create
-    # @post = Post.find(params[:id]) 
-    # @comment = Comment.create(params.require(:comment).permit(:text))
-    # @comment.post_id = @post.id 
-    # @comment.user_id = current_user.id
+    @post = Post.find(params[:post_id]) 
+    @comment = Comment.create(params.require(:comment).permit(:text))
+    @comment.post_id = @post.id 
+    @comment.user_id = current_user.id
     
-    @post = Post.find(params[:id]) 
-    @comment = @post.comments.create(:text, user_id: current_user.id, post_id: @post.id)
+    # @post = Post.find(params[:id]) 
+    # @comment = @post.comments.create(:text, user_id: current_user.id, post_id: @post.id)
     respond_to do |format|
       format.html do
         if @comment.save
-          redirect_to user_post_path(@post.user_id, @post.id)
+          redirect_to user_post_path(current_user.id, @post.id)
         else
-          redirect_to user_post_path(@post.user_id, @post.id)
+          redirect_to user_post_path(current_user.id, @post.id)
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,19 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:create]
     resources :likes, only: [:create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+# users
+# users/:id
+# users/:user_id/posts
+# users/:user_id/posts/:id
+
+# /posts 
+# /posts/id
+# posts/:post_id/
+# posts/id/comments
+# posts/id/likes
