4028d256b329d88af1115305d99fe1c0a5fb17f0
ali abbani <aliox@ahlam.com>
2022-01-26 16:38:44 +0200

    Add form to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,38 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.all
+    @posts = @user.posts
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id 
+    # 1 = 1
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = "Question saved successfully"
+          redirect_to user_posts_url
+        else
+          flash.now[:error] = "Error: Question could not be saved"
+          render :new 
+        end
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  resources :posts do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
