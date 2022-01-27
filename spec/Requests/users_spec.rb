644b80563e480fd0b1ff4d626a36bc6d4b13f9cf
ali abbani <aliox@ahlam.com>
2022-01-28 00:58:56 +0200

    Add spec test for user and post module validations

--- spec/Requests/users_spec.rb
+++ spec/Requests/users_spec.rb
@@ -2,42 +2,40 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before { get users_path }
-    it 'check if response status was correct' do
+    before { get '/users' }
+    it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'check if a correct template was rendered' do
-      expect(response).to render_template(:index)
+    it 'renders index template' do
+      expect(response).to render_template('index')
     end
 
     it 'does not render a different template' do
-      expect(response).to_not render_template(:show)
+      expect(response).to_not render_template('show')
     end
 
     it 'check if the response body includes correct placeholder text' do
-      expect(response.body).to include('users')
+      expect(response.body).to include('Here is the first page')
     end
   end
-end
 
-RSpec.describe 'Users', type: :request do
   describe 'GET /show' do
-    before { get user_path(5) }
-    it 'check if response status was correct' do
+    before { get '/users/1' }
+    it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'check if a correct template was rendered' do
-      expect(response).to render_template(:show)
+    it 'renders show template' do
+      expect(response).to render_template('show')
     end
 
     it 'does not render a different template' do
-      expect(response).to_not render_template(:index)
+      expect(response).to_not render_template('index')
     end
 
     it 'check if the response body includes correct placeholder text' do
-      expect(response.body).to include('Here is the page for a specific user')
+      expect(response.body).to include('Users#show')
     end
   end
 end
