21454d6e42ae50044f03aef6f00695d5f5ca1c24
azy64 <allysaidi64@gmail.com>
2022-02-02 16:24:37 +0100

    Fix the problem with the destroy method from CommentsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-
+  delete 'comment/:id', to: 'comments#destroy', as: 'destroy_comment'
   resources :users do
     resources :posts
   end
-
   resources :posts do
-    resources :comments, only: [:new, :create, :destroy]
+    resources :comments, only: [:new, :create]
     resources :likes, only: [:create, :destroy]
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
